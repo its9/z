a194e7ecc6b244250b1651b35350ff31cd7210af
its9 <itsover999999@gmail.com>
2025-01-16 00:31:07 +0530

    Update p7.cc

--- p7.cc
+++ p7.cc
@@ -1,99 +1,67 @@
-#include <fstream>
 #include "ns3/core-module.h"
-#include "ns3/applications-module.h"
+#include "ns3/network-module.h"
 #include "ns3/internet-module.h"
-#include "ns3/netanim-module.h"  // Include the NetAnim module
+#include "ns3/applications-module.h"
+#include "ns3/netanim-module.h"
 #include "ns3/point-to-point-module.h"
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("UdpClientServerExample");
+int main(int argc, char *argv[]) {
+    // Set simulation parameters
+    CommandLine cmd;
+    cmd.Parse(argc, argv);
 
-int main (int argc, char *argv[])
-{
-  // Declare variables used in command-line arguments
-  bool useV6 = false;
-  bool logging = true;
-  Address serverAddress;
+    // Create nodes
+    NodeContainer nodes;
+    nodes.Create(2);
 
-  CommandLine cmd (__FILE__);
-  cmd.AddValue ("useIpv6", "Use Ipv6", useV6);
-  cmd.AddValue ("logging", "Enable logging", logging);
-  cmd.Parse (argc, argv);
+    // Create network links
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
 
-  if (logging)
-    {
-      LogComponentEnable ("UdpClient", LOG_LEVEL_INFO);
-      LogComponentEnable ("UdpServer", LOG_LEVEL_INFO);
-    }
+    NetDeviceContainer devices;
+    devices = pointToPoint.Install(nodes);
 
-  NS_LOG_INFO ("Create nodes in the topology.");
-  NodeContainer n;
-  n.Create (2);
+    // Install Internet stack
+    InternetStackHelper stack;
+    stack.Install(nodes);
 
-  // Install internet stack
-  InternetStackHelper internet;
-  internet.Install (n);
+    // Assign IP addresses
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer interfaces = address.Assign(devices);
 
-  NS_LOG_INFO ("Create point-to-point link between the two nodes.");
-  PointToPointHelper p2p;
-  p2p.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
-  NetDeviceContainer devices = p2p.Install (n);
+    // Create a UDP server
+    
+    UdpServerHelper server(9);
+    ApplicationContainer serverApp = server.Install(nodes.Get(1));
+    serverApp.Start(Seconds(1.0));
+    serverApp.Stop(Seconds(10.0));
 
-  NS_LOG_INFO ("Assign IP Addresses.");
-  if (useV6 == false)
-    {
-      Ipv4AddressHelper ipv4;
-      ipv4.SetBase ("10.1.1.0", "255.255.255.0");
-      Ipv4InterfaceContainer i = ipv4.Assign (devices);
-      serverAddress = Address (i.GetAddress (1));
-    }
-  else
-    {
-      Ipv6AddressHelper ipv6;
-      ipv6.SetBase ("2001:0000:f00d:cafe::", Ipv6Prefix (64));
-      Ipv6InterfaceContainer i6 = ipv6.Assign (devices);
-      serverAddress = Address(i6.GetAddress (1,1));
-    }
+    // Create a UDP client
+    UdpClientHelper client(interfaces.GetAddress(1), 9);
+    client.SetAttribute("MaxPackets", UintegerValue(10));
+    client.SetAttribute("Interval", TimeValue(Seconds(1))); // 10 packets per second
+    client.SetAttribute("PacketSize", UintegerValue(1024));
 
-  NS_LOG_INFO ("Create UDP server application on node 1.");
-  uint16_t port = 4000;
-  UdpServerHelper server (port);
-  ApplicationContainer apps = server.Install (n.Get (1));
-  apps.Start (Seconds (1.0));
-  apps.Stop (Seconds (10.0));
+    ApplicationContainer clientApp = client.Install(nodes.Get(0));
+    clientApp.Start(Seconds(2.0));
+    clientApp.Stop(Seconds(10.0));
 
-  NS_LOG_INFO ("Create UDP client application on node 0 to send to node 1.");
-  uint32_t MaxPacketSize = 1024;
-  Time interPacketInterval = Seconds (0.05);
-  uint32_t maxPacketCount = 320;
-  UdpClientHelper client (serverAddress, port);
-  client.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
-  client.SetAttribute ("Interval", TimeValue (interPacketInterval));
-  client.SetAttribute ("PacketSize", UintegerValue (MaxPacketSize));
-  apps = client.Install (n.Get (0));
-  apps.Start (Seconds (2.0));
-  apps.Stop (Seconds (10.0));
+    // Enable ASCII tracing
+    AsciiTraceHelper ascii;
+    pointToPoint.EnableAsciiAll(ascii.CreateFileStream("udp-flow.tr"));
 
-  // Create animation object before running the simulation
-  AnimationInterface anim ("UdpClientServerExample.xml"); // This will create the XML file
+    // Enable NetAnim tracing
+    AnimationInterface anim("p7.xml");
+    anim.SetConstantPosition(nodes.Get(0), 0.0, 0.0);
+    anim.SetConstantPosition(nodes.Get(1), 50.0, 0.0);
 
-  // Set positions for the nodes in the animation with larger distance
-  anim.SetConstantPosition (n.Get(0), 2.0, 2.0);  // Position node 0 at (2, 2)
-  anim.SetConstantPosition (n.Get(1), 50.0, 50.0); // Position node 1 at (50, 50) to keep them far apart
+    // Run simulation
+    Simulator::Run();
+    Simulator::Destroy();
 
-  // Set the tracing for the UDP packets
-  AsciiTraceHelper ascii;
-  p2p.EnableAsciiAll(ascii.CreateFileStream("udp-trace.tr"));
-  p2p.EnablePcapAll("udp-pcap");
-
-  NS_LOG_INFO ("Run the simulation.");
-  Simulator::Run ();
-
-  // The XML file will be generated once the simulation finishes running
-  Simulator::Destroy ();
-
-  NS_LOG_INFO ("Done.");
+    return 0;
 }
-
